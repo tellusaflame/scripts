aca8aeba54bd16cce0cbfb9d2fc66311ff607059
tellusaflame <141950251+tellusaflame@users.noreply.github.com>
2024-08-09 22:56:23 +0300

    Update update_3x-ui.sh

--- update_3x-ui.sh
+++ update_3x-ui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# wget -O update_3x-ui.sh 
+# wget -O update_3x-ui.sh https://raw.githubusercontent.com/tellusaflame/scripts/main/update_3x-ui.sh?token=GHSAT0AAAAAACRKUAKCI42R33MFXQECNSCOZVWOSKQ
 
 cd ~/3x-ui
 docker compose down
